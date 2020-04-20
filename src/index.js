9757bf806dc56c8b237d4196170d5a3fafbac33d
Marcos Evaristo <marcos.wuh@gmail.com>
2020-04-20 00:57:47 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect(
-  'mongodb+srv://marcos:199465789Ab!@cluster0-mtaxd.mongodb.net/devsocialmedia?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect('mongodb+srv://marcos:199465789Ab!@cluster0-mtaxd.mongodb.net/devsocialmedia?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
